1e0e475d8996dae44d1ce8a410f5bde3a001a8a4
Ignat123 <ignat-nee@tut.by>
2014-09-08 22:51:43 +0300

    Трололо

--- src/main/java/core/GameActivity.java
+++ src/main/java/core/GameActivity.java
@@ -21,6 +21,7 @@ public class GameActivity extends BaseGameActivity {
     private List<GameObject> objectList;
     private Player player;
 
+    //Игнат
     @Override
     public Engine onLoadEngine() {
         final Camera camera = new Camera(0, 0, Utils.getScreenWidth(), Utils.getScreenHeight());
